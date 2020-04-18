2d0569186d4c8da805b87c41955d59455d438c88
Your Name <you@example.com>
2020-04-18 15:44:48 +0200

    Factorisation CSS

--- exo1_cookie/enteteAffichage.php
+++ exo1_cookie/enteteAffichage.php
@@ -22,41 +22,12 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="../menu.css" />
     <link rel="shortcut icon" href="../favicon.ico"  />
-    <style>
-		* {
-			box-sizing: border-box;
-		}
-		body{
-			margin:0;
-		}
-	
-	    /**********  ZONING ************************/
-
-		main{
-           
-		}
-
-		#contenu{
-			width: 70%;  
-			float: none;
-			padding: 25px 20px;
-			background: #ECECEA; 
-			border:none;
-			box-shadow:none;			
-			margin:auto;			
-		}
-		#contenu:after {
-			content: "";
-			display: table;
-			clear: both;
-		}
+    <style>		
 		
 		/* ne pas factoriser le footer car il y un bleme sur le footer de la page d'accueil */
 		
 		footer, .footer {
-			background-color: #74AFAD;
-			padding:1%;
-			text-align: left;		
+	
 			/* 
 			 * Fixer le footer au cas le contenu ne remplit pas la hauteur de l'écran
 			 * Mais si le contenu est suffisant, i fo virer ces trucs car ils  posent un probleme(voir page d'accueil) */
@@ -65,34 +36,7 @@
 			width:100%;
 			bottom:0 ;           		
 		}
-		
-		/************* Mise en page contenu *****************/
-		
-		.sortie{
-			  margin: 0 auto;
-			  width:84%; 			 			 		 
-		}
-		
-		button{
-			width:32.8%;		    
-			padding:2em 1em;
-		  
-			border:none;
-		}
-
-		.paragraphe{
-			margin: 1em auto;
-			width:84%;
-            float:left;			
-			border:1px solid #8c8b8b; 
-			padding:2% 5%  !important;
-
-			background: #f7f7f9;
-		}
-		.warning{
-			color:gray;
-			font:italic 1em calibri light;
-		}		
+			
     </style>	
 </head>
 <body>
--- exo1_cookie/enteteSaisie.php
+++ exo1_cookie/enteteSaisie.php
@@ -5,119 +5,18 @@
 	<link rel="stylesheet" type="text/css" href="../menu.css" />
 	<link rel="shortcut icon" href="../favicon.ico"  />
 	<style>
-		* {
-			box-sizing: border-box;
-		}
 	
-	/**********  ZONING ************************/
-
-		main{
-           
-		}
-
-		#contenu{
-			width: 70%;  
-			float: none;
-			padding: 25px 20px;
-			background: #ECECEA; 
-			border:none;
-			box-shadow:none;			
-			margin:auto;			
-		}
-		#contenu:after {
-			content: "";
-			display: table;
-			clear: both;
-		}
 		/* ne pas factoriser le footer car il y un bleme sur le footer de la page d'accueil */
 		footer, .footer {
-			background-color: #74AFAD;
-			padding:2% 1%;
-			text-align: left;
 			
 			/* 
 			 * Fixer le footer au cas le contenu ne remplit pas la hauteur de l'écran
 			 * Mais si le contenu est suffisant, i fo virer ces trucs car ils  posent un probleme(voir page d'accueil) */
-			*/
+			 */
 			position: absolute;
 			width:100%;
-			bottom:0;
-            		
-		}
-		
-	/************* Mise en page contenu *****************/
-		
-		.entree{
-			  margin: 0 auto;
-			  width:84%; 
-			  padding:0;
-			  background-color:white;   
-			  box-shadow:
-				0px 2px 2px 0px rgba(0, 0, 0, 0.5) inset,
-				0px 2px 2px 0px rgba(255, 255, 255, 0.5);
-			  border:1px solid #8c8b8b;
-		}
-		
-		button, .creuxDuBoutonEnvoyer, #envoyer {
-			width:33.33%;
-			float:right;
-			padding:2em;
-			border:none;
-			  box-shadow:
-			0px 2px 2px 0px rgba(0, 0, 0, 0.5) inset,
-			0px 2px 2px 0px rgba(255, 255, 255, 0.5);
-		}
-        .creuxDuBoutonEnvoyer{
-			width:100%;
-			padding:0.7em;
-			margin-bottom:1em;
-		}
-
-
-		#envoyer{
-			width:100%;		
-			padding:3% 1%;		
-		    background:#ececea; /* #74AFAD; */
-        	color:#115;
-			font-size:1.2em;
-			
-            /* Bouton envoyer */
-            padding-bottom:1.5em; 
-			box-shadow:
-			  0 15px 15px 0 #555 inset,
-			  0 -10px 1px 0 #999 inset;
-			background-color:#ddd;
-			
-			border:2px solid #bbb;
-			border-top:none ;			
-		}
-
-		
-		form{
-			width:100%;
-			padding:4em 0 0 0;
-			background:#558c89;/* #74AFAD;*/
-            color:#f1f1f1/* #ECECEA; */
-		}
-	
-        table{
-			width:100%;
-			padding-bottom:0;
-		}
-
-		table tr td {
-			padding:0 1em;
-		}
-
-		/* td conteneur des champs input */
-		table tr td ~ td {
-			width:80%;           		
-		}
-		input{
-			width:100% ;
-			padding:1.2em ;
-			margin:0.2em 0;
-		}
+			bottom:0;           		
+		}		
 	</style>	
 </head>
 <body>
