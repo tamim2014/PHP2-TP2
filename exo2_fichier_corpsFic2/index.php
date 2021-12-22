086fcd05ecb534d29bb440604bac6952a472dd9c
Your Name <you@example.com>
2021-12-22 02:41:20 +0100

    first commit

--- exo2_fichier_corpsFic2/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-     //session_start(); déja defini dans entete.php
-	 
-     include 'entete.php';
-     include 'corps.php';
-     include 'piedPage.php';
-?>
--- exo2_fichier_corpsFic2/piedPage.php
+++
@@ -1,7 +0,0 @@
-
-		<footer>
-			 <small style="color:#ECECEA;"><span style="color:gray;">2019 &copy; -</span> Master Handi</small>
-		<footer>
-	</main>
-</body>
-</html>  
